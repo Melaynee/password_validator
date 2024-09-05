17feea45188af02011d64123c5516b702f6f4b7b
Melaynee <melayneeyt@gmail.com>
2024-09-06 00:41:28 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PasswordStrengthComponent } from './password-strength/password-strength.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PasswordStrengthComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'password_validator';
+}
---
+++ src/app/password-strength/password-strength.component.ts
@@ -0,0 +1,60 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-password-strength',
+  standalone: true,
+  imports: [NgClass, FormsModule],
+  templateUrl: './password-strength.component.html',
+  styleUrl: './password-strength.component.css',
+})
+export class PasswordStrengthComponent {
+  password: string = '';
+  strength: 'empty' | 'short' | 'easy' | 'medium' | 'strong' = 'empty';
+  passwordVisability: 'password' | 'text' = 'password';
+
+  constructor() {}
+
+  setPasswordStrength(): void {
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasDigits = /\d/.test(this.password);
+    const hasSymbols =
+      /[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~`\_\-]/.test(
+        this.password
+      );
+
+    if (this.password.length === 0) {
+      this.strength = 'empty';
+    } else if (this.password.length < 8) {
+      this.strength = 'short';
+    } else if (hasLetters && hasDigits && hasSymbols) {
+      this.strength = 'strong';
+    } else if (
+      (hasLetters && hasDigits) ||
+      (hasDigits && hasSymbols) ||
+      (hasSymbols && hasLetters)
+    ) {
+      this.strength = 'medium';
+    } else {
+      this.strength = 'easy';
+    }
+  }
+
+  getClass(bar: number): string {
+    if (this.strength === 'empty') return '';
+    else if (this.strength === 'short') {
+      return 'red';
+    } else if (this.strength === 'easy') {
+      return bar === 1 ? 'red' : 'gray';
+    } else if (this.strength === 'medium') {
+      return bar < 3 ? 'yellow' : 'gray';
+    } else if (this.strength === 'strong') {
+      return 'green';
+    }
+    return 'gray';
+  }
+
+  
+}
